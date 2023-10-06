577582eb7458902251b805bcdc2b13df889952e9
Hector Martin <hectormartindama@gmail.com>
2023-10-06 13:13:55 +0200

    feat: first commit

---
+++ src/lib/data.js
@@ -0,0 +1,60 @@
+
+export const projectsData= [
+    {
+        title: 'Image Uploader',
+        description: 'Create an Image Uploader application. Use any front-end libraries of your choice. Create your API.',
+        tags: ['SvelteKit', 'Express', 'Node.js', 'Render', 'Cloudflare Pages'],
+        imgUrl: 'projectsImage/imageUploader2.png',
+        links: [{ name: 'Github', url: 'https://github.com/HectorMartinDama/Image-Uploader' }, { name: 'Live Site', url: 'https://image-uploader-c5i.pages.dev' }],
+        category: 'FullStack'
+    },
+    {
+        title: 'Rock Paper Scissors Game',
+        description:'this is a description',
+        tags: ['VanillaJS', 'Vite.js', 'Vercel'],
+        imgUrl: 'projectsImage/rock-paper-scissors-game.png',
+        links: [{ name: 'Github', url: 'https://github.com/HectorMartinDama/rock-paper-scissors-game' }, { name: 'Live Site', url: 'https://rock-paper-scissors-game-two-mu.vercel.app/' }],
+        category: 'Frontend'
+    },
+    {
+        title: 'Clon the Unsplash',
+        description: 'this is a description',
+        tags: ['Sveltekit', 'Express', 'Node.js', 'Vite.js', 'Vercel', 'fl0', 'MongoDB', 'Firebase'],
+        imgUrl: 'projectsImage/myUnsplash.png',
+        links: [{ name: 'Github', url: '' }, { name: 'Live Site', url: '' }],
+    }
+];
+
+
+export const skillsData= [
+    'Angular',
+    'Sveltekit',
+    'Astro',
+    'Testing Library', 
+    'Vitest / Jest',
+    'Cypress',
+    'Motion one',
+    'ESLint',
+    'Prettier',
+    'Tailwind CSS',
+    'Sass',
+    'Pattern 7-1',
+    'Node.js',
+    'Express',
+    'Supertest',
+    'Graphql',
+    'JWT',
+    'Swagger',
+    'Postman',
+    'MySQL',
+    'SQLite',
+    'MongoDB',
+    'Firebase',
+    'Redis',
+    'Git',
+    'Github Actions (CI & CD)',
+    'Docker',
+    'Docker-Compose',
+    'Cloudflare Workers',
+    'Vercel'
+];
