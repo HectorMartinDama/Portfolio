64bd26926a5ecee4947c2d84ce56ad7b23b89cb9
Hector Martin <hectormartindama@gmail.com>
2023-11-07 20:11:35 +0100

    style: new version of the page

--- src/lib/data.js
+++ src/lib/data.js
@@ -4,7 +4,7 @@ export const projectsData= [
         title: 'Image Uploader',
         description: 'Create an Image Uploader application. Use any front-end libraries of your choice. Create your API.',
         tags: ['SvelteKit', 'Express', 'Node.js', 'Render', 'Cloudflare Pages'],
-        imgUrl: 'projectsImage/imageUploader2.png',
+        imgUrl: 'projectsImage/imageUploader2.webp',
         links: [{ name: 'Github', url: 'https://github.com/HectorMartinDama/Image-Uploader' }, { name: 'Live Site', url: 'https://image-uploader-c5i.pages.dev' }],
         category: 'FullStack'
     },
@@ -12,7 +12,7 @@ export const projectsData= [
         title: 'Rock Paper Scissors Game',
         description:'this is a description',
         tags: ['VanillaJS', 'Vite.js', 'Vercel'],
-        imgUrl: 'projectsImage/rock-paper-scissors-game.png',
+        imgUrl: 'projectsImage/rock-paper-scissors-game.webp',
         links: [{ name: 'Github', url: 'https://github.com/HectorMartinDama/rock-paper-scissors-game' }, { name: 'Live Site', url: 'https://rock-paper-scissors-game-two-mu.vercel.app/' }],
         category: 'Frontend'
     },
@@ -20,13 +20,33 @@ export const projectsData= [
         title: 'Clon the Unsplash',
         description: 'this is a description',
         tags: ['Sveltekit', 'Express', 'Node.js', 'Vite.js', 'Vercel', 'fl0', 'MongoDB', 'Firebase'],
-        imgUrl: 'projectsImage/myUnsplash.png',
+        imgUrl: 'projectsImage/myUnsplash.webp',
         links: [{ name: 'Github', url: '' }, { name: 'Live Site', url: '' }],
     }
 ];
 
 
+export const certificationsData= [
+    {
+        name: 'Redis for JavaScript Developers',
+        img: 'icons/redis.svg',
+        url: 'https://university.redis.com/certificates/72a32b5f73b6459bbc6f3a448de8add4'
+    },
+    {
+        name: 'Curso de Testing en Frontend',
+        img: 'icons/codely.jpeg',
+        url: 'https://pro.codely.com/library/testing-en-frontend-65916/196940/about/'
+    },
+    {
+        name: 'ITIL 4 Foundation (in progress...)',
+        img: 'https://eu-images.contentstack.com/v3/assets/blt637b065823946b12/blt7ddb9fc285023638/60ffc2997e10fe36b4a0823a/itile-logo.svg?width=undefined&height=undefined&fit=bounds&crop=undefined%2Csmart&auto=webp&quality=60&dpr=2',
+        url: 'https://www.axelos.com/certifications/itil-service-management/itil-4-foundation'
+    }
+];
+
+
 export const skillsData= [
+    'TypeScript',
     'Angular',
     'Sveltekit',
     'Astro',
