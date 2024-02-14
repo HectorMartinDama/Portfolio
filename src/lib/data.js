4789258ded926d674b2b5ddd10ec8422140d7f40
Hector Martin <hectormartindama@gmail.com>
2024-02-14 19:15:19 +0100

    initial commit

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,85 +1,100 @@
-
-export const projectsData= [
-    {
-        title: 'Image Uploader',
-        description: 'Create an Image Uploader application. Use any front-end libraries of your choice. Create your API.',
-        tags: ['SvelteKit', 'Express', 'Node.js', 'Render', 'Cloudflare Pages'],
-        imgUrl: 'projectsImage/imageUploader2.webp',
-        links: [{ name: 'Github', url: 'https://github.com/HectorMartinDama/Image-Uploader' }, { name: 'Live Site', url: 'https://image-uploader-c5i.pages.dev' }],
-        category: 'FullStack'
-    },
-    {
-        title: 'Rock Paper Scissors Game',
-        description:'this is a description',
-        tags: ['VanillaJS', 'Vite.js', 'Vercel'],
-        imgUrl: 'projectsImage/rock-paper-scissors-game.webp',
-        links: [{ name: 'Github', url: 'https://github.com/HectorMartinDama/rock-paper-scissors-game' }, { name: 'Live Site', url: 'https://rock-paper-scissors-game-two-mu.vercel.app/' }],
-        category: 'Frontend'
-    },
-    {
-        title: 'Clon the Unsplash',
-        description: 'this is a description',
-        tags: ['Sveltekit', 'Express', 'Node.js', 'Vite.js', 'Vercel', 'fl0', 'MongoDB', 'Firebase'],
-        imgUrl: 'projectsImage/myUnsplash.webp',
-        links: [{ name: 'Github', url: '' }, { name: 'Live Site', url: '' }],
-    }
+export const projectsData = [
+  {
+    title: 'Image Uploader',
+    description: 'Create an Image Uploader application. Use any front-end libraries of your choice. Create your API.',
+    tags: ['SvelteKit', 'Express', 'Node.js', 'Render', 'Cloudflare Pages'],
+    imgsUrl: [
+      { name: 'desktop', url: 'projectsImage/imageUploader/desktop.webp' },
+      { name: 'mobile', url: 'projectsImage/imageUploader/mobile.webp' }
+    ],
+    links: [
+      { name: 'Github', url: 'https://github.com/HectorMartinDama/Image-Uploader' },
+      { name: 'Live Site', url: 'https://image-uploader-c5i.pages.dev' }
+    ],
+    category: 'FullStack'
+  },
+  {
+    title: 'Rock Paper Scissors Game',
+    description: 'this is a description',
+    tags: ['VanillaJS', 'Vite.js', 'Vercel'],
+    imgsUrl: [
+      { name: 'desktop', url: 'projectsImage/rock-paper-scissors-game/desktop.webp' },
+      { name: 'mobile', url: 'projectsImage/rock-paper-scissors-game/mobile.webp' }
+    ],
+    links: [
+      { name: 'Github', url: 'https://github.com/HectorMartinDama/rock-paper-scissors-game' },
+      { name: 'Live Site', url: 'https://rock-paper-scissors-game-two-mu.vercel.app/' }
+    ],
+    category: 'Frontend'
+  },
+  {
+    title: 'Clon the Unsplash',
+    description: 'this is a description',
+    tags: ['Sveltekit', 'Express', 'Node.js', 'Vite.js', 'Vercel', 'fl0', 'MongoDB', 'Firebase'],
+    imgsUrl: [
+      { name: 'desktop', url: 'projectsImage/myUnsplash.webp' },
+      { name: 'mobile', url: 'projectsImage/myUnsplash.webp' }
+    ],
+    links: [
+      { name: 'Github', url: '' },
+      { name: 'Live Site', url: '' }
+    ]
+  }
 ];
 
-
-export const certificationsData= [
-    {
-        name: 'Redis for JavaScript Developers',
-        img: 'icons/redis.svg',
-        url: 'https://university.redis.com/certificates/72a32b5f73b6459bbc6f3a448de8add4'
-    },
-    {
-        name: 'Curso de Testing en Frontend',
-        img: 'icons/codely.jpeg',
-        url: 'https://drive.google.com/file/d/13VY_-tWLlpeCifIw2qnEjEY4cYb0klQk/view?usp=sharing'
-    },
-    {
-        name: 'Principios SOLID Aplicados',
-        img: 'icons/codely.jpeg',
-        url: 'https://drive.google.com/file/d/18B87DopOsBVqY8XZU7zk8DfLkMSd9UPL/view?usp=sharing'
-    },
-    {
-        name: 'ITIL 4 Foundation (in progress...)',
-        img: 'https://eu-images.contentstack.com/v3/assets/blt637b065823946b12/blt7ddb9fc285023638/60ffc2997e10fe36b4a0823a/itile-logo.svg?width=undefined&height=undefined&fit=bounds&crop=undefined%2Csmart&auto=webp&quality=60&dpr=2',
-        url: 'https://www.axelos.com/certifications/itil-service-management/itil-4-foundation'
-    }
+export const certificationsData = [
+  {
+    name: 'Redis for JavaScript Developers',
+    img: 'icons/redis.svg',
+    url: 'https://university.redis.com/certificates/72a32b5f73b6459bbc6f3a448de8add4'
+  },
+  {
+    name: 'Curso de Testing en Frontend',
+    img: 'icons/codely.jpeg',
+    url: 'https://drive.google.com/file/d/13VY_-tWLlpeCifIw2qnEjEY4cYb0klQk/view?usp=sharing'
+  },
+  {
+    name: 'Principios SOLID Aplicados',
+    img: 'icons/codely.jpeg',
+    url: 'https://drive.google.com/file/d/18B87DopOsBVqY8XZU7zk8DfLkMSd9UPL/view?usp=sharing'
+  },
+  {
+    name: 'ITIL 4 Foundation (in progress...)',
+    img: 'https://eu-images.contentstack.com/v3/assets/blt637b065823946b12/blt7ddb9fc285023638/60ffc2997e10fe36b4a0823a/itile-logo.svg?width=undefined&height=undefined&fit=bounds&crop=undefined%2Csmart&auto=webp&quality=60&dpr=2',
+    url: 'https://www.axelos.com/certifications/itil-service-management/itil-4-foundation'
+  }
 ];
 
-
-export const skillsData= [
-    'TypeScript',
-    'Angular',
-    'Sveltekit',
-    'Astro',
-    'Testing Library', 
-    'Vitest / Jest',
-    'Cypress',
-    'Motion one',
-    'ESLint',
-    'Prettier',
-    'Tailwind CSS',
-    'Sass',
-    'Pattern 7-1',
-    'Node.js',
-    'Express',
-    'Supertest',
-    'Graphql',
-    'JWT',
-    'Swagger',
-    'Postman',
-    'MySQL',
-    'SQLite',
-    'MongoDB',
-    'Firebase',
-    'Redis',
-    'Git',
-    'Github Actions (CI & CD)',
-    'Docker',
-    'Docker-Compose',
-    'Cloudflare Workers',
-    'Vercel'
+export const skillsData = [
+  'TypeScript',
+  'Angular',
+  'Sveltekit',
+  'Astro',
+  'Testing Library',
+  'Vitest / Jest',
+  'Cypress',
+  'Motion one',
+  'ESLint',
+  'Prettier',
+  'Tailwind CSS',
+  'Sass',
+  'Pattern 7-1',
+  'Node.js',
+  'Express',
+  'Supertest',
+  'Graphql',
+  'JWT',
+  'Swagger',
+  'Postman',
+  'MySQL',
+  'SQLite',
+  'MongoDB',
+  'Firebase',
+  'Redis',
+  'Git',
+  'Github Actions (CI & CD)',
+  'Docker',
+  'Docker-Compose',
+  'Cloudflare Workers',
+  'Vercel'
 ];
