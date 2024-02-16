b2049147d2aac1c21b9f1d3d1c58fe3708810bc6
Hector Martin <hectormartindama@gmail.com>
2024-02-16 11:05:54 +0100

    feat: new version

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,71 +1,23 @@
-export const projectsData = [
+export const PROJECTS = [
   {
-    title: 'Image Uploader',
-    description: 'Create an Image Uploader application. Use any front-end libraries of your choice. Create your API.',
-    tags: ['SvelteKit', 'Express', 'Node.js', 'Render', 'Cloudflare Pages'],
-    imgsUrl: [
-      { name: 'desktop', url: 'projectsImage/imageUploader/desktop.webp' },
-      { name: 'mobile', url: 'projectsImage/imageUploader/mobile.webp' }
-    ],
-    links: [
-      { name: 'Github', url: 'https://github.com/HectorMartinDama/Image-Uploader' },
-      { name: 'Live Site', url: 'https://image-uploader-c5i.pages.dev' }
-    ],
-    category: 'FullStack'
+    title: 'Image Uploader - Sube tu imagen a internet',
+    description:
+      'Subida de una imagen a un servidor mediante una API con acceso público a través de una URL. Desarrollado desde cero con SvelteKit y Express. Desplegado en Cloudflare Pages y Render.',
+    link: 'https://image-uploader-c5i.pages.dev',
+    github: 'https://github.com/HectorMartinDama/Image-Uploader',
+    image: '/projectsImage/imageUploader.webp'
   },
   {
     title: 'Rock Paper Scissors Game',
-    description: 'this is a description',
-    tags: ['VanillaJS', 'Vite.js', 'Vercel'],
-    imgsUrl: [
-      { name: 'desktop', url: 'projectsImage/rock-paper-scissors-game/desktop.webp' },
-      { name: 'mobile', url: 'projectsImage/rock-paper-scissors-game/mobile.webp' }
-    ],
-    links: [
-      { name: 'Github', url: 'https://github.com/HectorMartinDama/rock-paper-scissors-game' },
-      { name: 'Live Site', url: 'https://rock-paper-scissors-game-two-mu.vercel.app/' }
-    ],
-    category: 'Frontend'
-  },
-  {
-    title: 'Clon the Unsplash',
-    description: 'this is a description',
-    tags: ['Sveltekit', 'Express', 'Node.js', 'Vite.js', 'Vercel', 'fl0', 'MongoDB', 'Firebase'],
-    imgsUrl: [
-      { name: 'desktop', url: 'projectsImage/myUnsplash.webp' },
-      { name: 'mobile', url: 'projectsImage/myUnsplash.webp' }
-    ],
-    links: [
-      { name: 'Github', url: '' },
-      { name: 'Live Site', url: '' }
-    ]
+    description:
+      'Juego interactivo con diseño responsive para todos los dispositivos. Creado desde cero con Vanilla.js. Desplegado en Vercel.',
+    link: 'https://rock-paper-scissors-game-two-mu.vercel.app/',
+    github: 'https://github.com/HectorMartinDama/rock-paper-scissors-game',
+    image: '/projectsImage/rock-paper-scissors.webp'
   }
 ];
 
-export const certificationsData = [
-  {
-    name: 'Redis for JavaScript Developers',
-    img: 'icons/redis.svg',
-    url: 'https://university.redis.com/certificates/72a32b5f73b6459bbc6f3a448de8add4'
-  },
-  {
-    name: 'Curso de Testing en Frontend',
-    img: 'icons/codely.jpeg',
-    url: 'https://drive.google.com/file/d/13VY_-tWLlpeCifIw2qnEjEY4cYb0klQk/view?usp=sharing'
-  },
-  {
-    name: 'Principios SOLID Aplicados',
-    img: 'icons/codely.jpeg',
-    url: 'https://drive.google.com/file/d/18B87DopOsBVqY8XZU7zk8DfLkMSd9UPL/view?usp=sharing'
-  },
-  {
-    name: 'ITIL 4 Foundation (in progress...)',
-    img: 'https://eu-images.contentstack.com/v3/assets/blt637b065823946b12/blt7ddb9fc285023638/60ffc2997e10fe36b4a0823a/itile-logo.svg?width=undefined&height=undefined&fit=bounds&crop=undefined%2Csmart&auto=webp&quality=60&dpr=2',
-    url: 'https://www.axelos.com/certifications/itil-service-management/itil-4-foundation'
-  }
-];
-
-export const skillsData = [
+export const SKILLS = [
   'TypeScript',
   'Angular',
   'Sveltekit',
