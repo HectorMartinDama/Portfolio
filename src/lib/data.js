c47c1b163aa16ba63c2d01595d75c1306164b603
Hector Martin <hectormartindama@gmail.com>
2024-05-03 13:18:12 +0200

    feat: add new project

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,5 +1,15 @@
 export const PROJECTS = [
   {
+    title: 'Thullo - Comparte, gestiona tus tableros',
+    description:
+      'App basada en Trello, desarrollada con Sveltekit en el frontend y en el backend con Express aplicando DDD, Hexagonal Architecture, Event-Drive Architecture, Gestion de colas RabbitMQ, AWS SES y S3.',
+    link: 'https://thullo.pages.dev',
+    frontend: 'https://github.com/HectorMartinDama/Thullo',
+    backend: 'https://github.com/HectorMartinDama/Thullo-backend',
+    image: '/projectsImage/thullo.webp',
+    article: 'https://github.com/HectorMartinDama/Thullo'
+  },
+  {
     title: 'Image Uploader - Sube tu imagen a internet',
     description:
       'Subida de una imagen a un servidor mediante una API con acceso público a través de una URL. Desarrollado desde cero con SvelteKit y Express. Desplegado en Cloudflare Pages y Render.',
