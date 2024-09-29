3fbca65d76f742f8cf8b5bb989d13ce0ad993dbd
Hector Martin <hectormartindama@gmail.com>
2024-09-29 14:16:36 +0200

    feat: add a server-island in the AllBlogPosts component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   integrations: [tailwind()],
+  env: {
+    schema: {
+      BLOG_API_ENDPOINT: envField.string({
+        context: "server",
+        access: "secret",
+      }),
+    },
+  },
 });
