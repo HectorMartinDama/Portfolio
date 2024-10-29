6def09a4360c486a2e36640661041657c0ba6030
Hector Martin <hectormartindama@gmail.com>
2024-10-29 12:32:25 +0100

    fix: add vercel serverless adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-import vercelStatic from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  adapter: vercelStatic({
+  output: "server",
+  adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
